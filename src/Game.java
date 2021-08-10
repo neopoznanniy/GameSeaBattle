c639d4312af43b8c351ed8e22deaf02afdb302e4
neopoznanniy <smirnovmh8@gmail.com>
2021-08-10 14:52:49 +0300

    SeaBattle v1.0

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Game {
-    public static void main(String[] args){
         private GameHelper helper = new GameHelper();
         private ArrayList<Ship> shipsList = new ArrayList<Ship>();
         private int numOfGuess = 0;
@@ -21,7 +20,7 @@ public class Game {
             //Выводим инструкции для пользователя
             System.out.println("Ваша цель потопить три корабля.");
             System.out.println("Попытайтесь потопить их за минимальное количество ходов.");
-            System.out.println("А не то они потопят вас.");
+            System.out.println("Пока они не разрушили вашу базу.");
 
             //Используя вспомогательный класс, задаем размещение кораблям
             for(Ship shipToSet : shipsList){
@@ -30,12 +29,42 @@ public class Game {
             }
         }
         private void startPlaying(){
+            //Принимаем ход от пользователя, пока все корабли не будут потоплены
             while(!shipsList.isEmpty()){
                 String userGuess = helper.getUserInput("Сделайте ход");
                 checkUserGuess(userGuess);
             }
             finishGame();
         }
-
-    }
+        private void checkUserGuess(String userGuess){
+            //Опрашиваем каждый корабль на наличие попадания.
+            numOfGuess++;
+            String result = "Мимо";
+            for(Ship shipTest: shipsList){
+                result = shipTest.checkYourSelf(userGuess);
+                if(result.equals("Попал")) {
+                    break;
+                }
+                if(result.equals("Потопил")){
+                    shipsList.remove(shipTest);
+                    break;
+                }
+            }
+            System.out.println(result);
+        }
+        private void finishGame(){
+            System.out.println("УРА!!! Все корабли потоплены.");
+            if(numOfGuess<=18){
+                System.out.println("Это заняло у Вас всего "+numOfGuess+" попыток.");
+                System.out.println("Вы успели затопить корабли, до того как они разрушат вашу базу.");
+            }else{
+                System.out.println("Однако, это заняло у вас довольно много времени. "+numOfGuess+" попыток.");
+                System.out.println("Ваша база лежит в руинах. :'(");
+            }
+        }
+        public static void main(String[] args){
+            Game game = new Game();
+            game.setUpGame();
+            game.startPlaying();
+        }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 
 public class Ship {
     private ArrayList<String> locationCells;
+    private String name;
 
-    String checkYourSelf(String stringGuess){
-
+    String checkYourSelf(String userInput){
         String result = "Мимо";
         int index = locationCells.indexOf(userInput);
-
         if(index>=0){
             locationCells.remove(index);
             if(locationCells.isEmpty()){
                 result="Потопил";
+                System.out.println(name+" затоплен!");
             }else{
                 result="Попал";
             }
@@ -23,4 +23,5 @@ public class Ship {
     public void setLocationCells(ArrayList<String> loc){
         this.locationCells = loc;
     }
+    public void setName (String n) {name = n;}
 }
